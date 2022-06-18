5d4ef0c5ec6018eb38a1a117859c27d1cb0bf588
KangKung <d458996@gmail.com>
2022-06-18 13:42:30 +0700

    first commit

---
+++ Library.lua
@@ -0,0 +1,183 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local UWU = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local Title = Instance.new("TextLabel")
+local Version = Instance.new("TextLabel")
+local Exit = Instance.new("TextButton")
+local Function6 = Instance.new("TextButton")
+local UICorner = Instance.new("UICorner")
+local Function5 = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+local Function4 = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local Function3 = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local Function2 = Instance.new("TextButton")
+local UICorner_5 = Instance.new("UICorner")
+local Function1 = Instance.new("TextButton")
+local UICorner_6 = Instance.new("UICorner")
+
+--Properties:
+
+UWU.Name = "UWU"
+UWU.Parent = game.CoreGui
+
+Frame.Parent = UWU
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BorderSizePixel = 3
+Frame.Position = UDim2.new(0.315217376, 0, 0.281287253, 0)
+Frame.Size = UDim2.new(0, 510, 0, 298)
+
+Title.Name = "Title"
+Title.Parent = Frame
+Title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Title.BackgroundTransparency = 1.000
+Title.BorderSizePixel = 0
+Title.Position = UDim2.new(0.135805622, 0, -0.0025357753, 0)
+Title.Size = UDim2.new(0, 374, 0, 36)
+Title.Font = Enum.Font.FredokaOne
+Title.Text = "Royx Combo"
+Title.TextColor3 = Color3.fromRGB(0, 0, 0)
+Title.TextSize = 32.000
+Title.TextStrokeColor3 = Color3.fromRGB(85, 85, 255)
+Title.TextStrokeTransparency = 0.800
+
+Version.Name = "Version"
+Version.Parent = Frame
+Version.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Version.BackgroundTransparency = 1.000
+Version.BorderSizePixel = 0
+Version.Position = UDim2.new(0.627450943, 0, 0.912751675, 0)
+Version.Size = UDim2.new(0, 181, 0, 26)
+Version.Font = Enum.Font.Highway
+Version.Text = "Version : 1.0.0"
+Version.TextColor3 = Color3.fromRGB(0, 0, 0)
+Version.TextSize = 18.000
+Version.TextWrapped = true
+Version.TextXAlignment = Enum.TextXAlignment.Right
+
+Exit.Name = "Exit"
+Exit.Parent = Frame
+Exit.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Exit.BackgroundTransparency = 1.000
+Exit.BorderSizePixel = 0
+Exit.Position = UDim2.new(0.907051802, 0, -0.00312447548, 0)
+Exit.Size = UDim2.new(0, 47, 0, 35)
+Exit.Font = Enum.Font.FredokaOne
+Exit.Text = "X"
+Exit.TextColor3 = Color3.fromRGB(0, 0, 0)
+Exit.TextSize = 24.000
+
+Function6.Name = "Function 6"
+Function6.Parent = Frame
+Function6.BackgroundColor3 = Color3.fromRGB(255, 170, 255)
+Function6.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Function6.BorderSizePixel = 2
+Function6.Position = UDim2.new(0.543137252, 0, 0.718120813, 0)
+Function6.Size = UDim2.new(0, 200, 0, 50)
+Function6.Font = Enum.Font.SourceSansBold
+Function6.Text = "Not Ready"
+Function6.TextColor3 = Color3.fromRGB(0, 0, 0)
+Function6.TextSize = 24.000
+Function6.TextWrapped = true
+
+UICorner.CornerRadius = UDim.new(0, 30)
+UICorner.Parent = Function6
+
+Function5.Name = "Function 5"
+Function5.Parent = Frame
+Function5.BackgroundColor3 = Color3.fromRGB(255, 170, 255)
+Function5.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Function5.BorderSizePixel = 2
+Function5.Position = UDim2.new(0.543137252, 0, 0.456375837, 0)
+Function5.Size = UDim2.new(0, 200, 0, 50)
+Function5.Font = Enum.Font.SourceSansBold
+Function5.Text = "Not Ready"
+Function5.TextColor3 = Color3.fromRGB(0, 0, 0)
+Function5.TextSize = 24.000
+Function5.TextWrapped = true
+
+UICorner_2.CornerRadius = UDim.new(0, 30)
+UICorner_2.Parent = Function5
+
+Function4.Name = "Function 4"
+Function4.Parent = Frame
+Function4.BackgroundColor3 = Color3.fromRGB(255, 170, 255)
+Function4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Function4.BorderSizePixel = 2
+Function4.Position = UDim2.new(0.543137252, 0, 0.204697981, 0)
+Function4.Size = UDim2.new(0, 200, 0, 50)
+Function4.Font = Enum.Font.SourceSansBold
+Function4.Text = "Not Ready"
+Function4.TextColor3 = Color3.fromRGB(0, 0, 0)
+Function4.TextSize = 24.000
+Function4.TextWrapped = true
+
+UICorner_3.CornerRadius = UDim.new(0, 30)
+UICorner_3.Parent = Function4
+
+Function3.Name = "Function 3"
+Function3.Parent = Frame
+Function3.BackgroundColor3 = Color3.fromRGB(255, 170, 255)
+Function3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Function3.BorderSizePixel = 2
+Function3.Position = UDim2.new(0.0196078438, 0, 0.718120813, 0)
+Function3.Size = UDim2.new(0, 200, 0, 50)
+Function3.Font = Enum.Font.SourceSansBold
+Function3.Text = "Not Ready"
+Function3.TextColor3 = Color3.fromRGB(0, 0, 0)
+Function3.TextSize = 24.000
+Function3.TextWrapped = true
+
+UICorner_4.CornerRadius = UDim.new(0, 30)
+UICorner_4.Parent = Function3
+
+Function2.Name = "Function 2"
+Function2.Parent = Frame
+Function2.BackgroundColor3 = Color3.fromRGB(255, 170, 255)
+Function2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Function2.BorderSizePixel = 2
+Function2.Position = UDim2.new(0.0196078438, 0, 0.456375837, 0)
+Function2.Size = UDim2.new(0, 200, 0, 50)
+Function2.Font = Enum.Font.SourceSansBold
+Function2.Text = "Not Ready"
+Function2.TextColor3 = Color3.fromRGB(0, 0, 0)
+Function2.TextSize = 24.000
+Function2.TextWrapped = true
+
+UICorner_5.CornerRadius = UDim.new(0, 30)
+UICorner_5.Parent = Function2
+
+Function1.Name = "Function 1"
+Function1.Parent = Frame
+Function1.BackgroundColor3 = Color3.fromRGB(255, 170, 255)
+Function1.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Function1.BorderSizePixel = 2
+Function1.Position = UDim2.new(0.0196078438, 0, 0.204697981, 0)
+Function1.Size = UDim2.new(0, 200, 0, 50)
+Function1.Font = Enum.Font.SourceSansBold
+Function1.Text = "Not Ready"
+Function1.TextColor3 = Color3.fromRGB(0, 0, 0)
+Function1.TextSize = 24.000
+Function1.TextWrapped = true
+
+UICorner_6.CornerRadius = UDim.new(0, 30)
+UICorner_6.Parent = Function1
+local Library = { Button = {
+    F1 = Function1,
+    F2 = Function2,
+    F3 = Function3,
+    F4 = Function4,
+    F5 = Function5,
+    F6 = Function6
+}, Version = Version };
+
+function Library:AddCombo(Button, Callback)
+    Button.MouseButton1Down:Connect(Callback);
+end
+
+return Library;
